965154e60f1bbb98989d12225effe02a1aeb242b
SyoiWatsu <r.nakano.job@gmail.com>
2021-03-06 23:53:10 +0000

    Plan一覧画面_作戦memo記入

--- app/controllers/v1/plans_controller.rb
+++ app/controllers/v1/plans_controller.rb
@@ -35,7 +35,56 @@ class V1::PlansController < ApplicationController
     end
   end
 
-  
+  def index
+    # plansData = [
+    #   {
+    #     user_id: 1,
+    #     user_name: "hoge"
+    #     plans: [
+    #       {
+    #         plan_id: 1,
+    #         title: "hoge1",
+    #       },
+    #       {
+    #         plan_id: 2,
+    #         title: "hoge2",
+    #       },
+    #     ]
+    #   },
+    #   {
+    #     user_id: 2,
+    #     user_name: "mage",
+    #     plans: [
+    #       {
+    #         plan_id: 3,
+    #         title: "mage1",
+    #       },
+    #       {
+    #         plan_id: 4
+    #         title: "mage4",
+    #       },
+    #     ]
+    #   },
+    # ]
+    # 
+    # ↑ こんな感じのデータを返したい
+
+    # その為には
+    # ① Plan全件取得
+    # ② IDでグルーピング
+    # ていう感じで受け取った後にこっちでデータ加工すればいいのかな？
+    # それとも取得時点でここの処理をDB側に指示して願いすることできるのかな？
+    # なんとなく後者な気がするけど、find_by_sql使えばいいのかなあ
+    # → https://bit.ly/3rpLByK
+    # 
+    # DB側にグルーピング任せた時に返ってくる値ってどういう形で返ってくるんだろう。
+    # そのへんも全然わからんな。ちょっと調べて分からなかったら
+    # 全件取得からのこっちでグルーピングやっちゃうか
+    # 　↓
+    # group_by使うとこういう感じでハッシュで返ってくるらしい！イケる。
+    # → https://bit.ly/3v3RjZ4
+
+  end
 
 
   def show
