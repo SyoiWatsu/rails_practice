29c9db820e3ec4ca33af969ea7169347e26ffeb3
SyoiWatsu <r.nakano.job@gmail.com>
2021-03-08 13:51:40 +0000

    Plan一覧画面_titleクリックでPlan詳細に遷移するように

--- src/pages/MatcherClone/PlanIndex.vue
+++ src/pages/MatcherClone/PlanIndex.vue
@@ -5,7 +5,7 @@
       <div v-for="planData in plansData">
         <p>▶︎ Plans made by user_id : {{ planData[0].user_id }}</p>
         <ul>
-          <li v-for="(plan, index) in planData">Plan_{{ index + 1 }} : {{ plan.title }}</li>
+          <li v-for="(plan, index) in planData" v-on:click="showDetail(plan.id)">Plan_{{ index + 1 }} : {{ plan.title }}</li>
         </ul>
       </div>
     </div>
@@ -35,16 +35,20 @@ export default {
     this.fetchPlansData();
   },
   methods: {
+    
+    //Plan詳細を表示する関数 
+    showDetail : function(planId){
+      const baseUrl = "matcher-clone/plans/";
+      this.$router.push(baseUrl + planId);
+    },
+
+    // Planのデータを取得する関数
     async fetchPlansData() {
       const response = await axios.get('/api/v1/plans');
       this.plansData = response.data.plansData;
-      console.log(this.plansData);
-      console.log("length : " + Object.keys(this.plansData).length);
-      console.log("40 : " + this.plansData["40"][0].id);
-
-      // memo
-      // オブジェクトのv-for　→ https://bit.ly/2Oxfm1O
-      
+      // console.log(this.plansData);
+      // console.log("length : " + Object.keys(this.plansData).length);
+      // console.log("40 : " + this.plansData["40"][0].id);
     },
   },
 }
