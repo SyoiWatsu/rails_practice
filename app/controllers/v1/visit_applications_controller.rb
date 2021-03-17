322e56cb9387162619ca51220af22fad873aec12
SyoiWatsu <r.nakano.job@gmail.com>
2021-03-17 02:38:27 +0000

    feat: ログインユーザーのみがプラン申請を行えるようにするため、before_actionでユーザーのログイン済みかどうか確認

--- app/controllers/v1/visit_applications_controller.rb
+++ app/controllers/v1/visit_applications_controller.rb
@@ -1,6 +1,6 @@
 class V1::VisitApplicationsController < ApplicationController
 
-  # before_action :authenticate_user!, only: [:create]
+  before_action :authenticate_user!, only: [:create]
 
   def create
     p "here!!!!!!!!!!!"
