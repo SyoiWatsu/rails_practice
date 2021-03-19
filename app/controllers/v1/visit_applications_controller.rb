2604d1e150b582d9b1cef4ff9a3dab89bb57c1f1
SyoiWatsu <r.nakano.job@gmail.com>
2021-03-19 07:16:14 +0000

    feat: Plan申請_ApplyボタンでVisitApplicationテーブルに新しいレコードが作られるように

--- app/controllers/v1/visit_applications_controller.rb
+++ app/controllers/v1/visit_applications_controller.rb
@@ -11,19 +11,30 @@ class V1::VisitApplicationsController < ApplicationController
     applicant_id = current_user.id #プラン申込者のuser_id 
     status = "wating"
 
-    # ↑ まずvisit_applicationsテーブルのカラム変えないといけない。
-    # → 再度マイグレーションするマン
-
     # ②受け取った値を元にVisit_applicationのインスタンス作成
-    # new_visit_application = Visit_application.new()
+    new_visit_application = VisitApplication.new()
+    new_visit_application.applicant_id = applicant_id
+    new_visit_application.authorizer_id = authorizer_id
+    new_visit_application.plan_id = plan_id
+    new_visit_application.status = status
 
     # ③保存
+    if new_visit_application.save
 
+      obj = {
+        msg: "success !!",
+        new_visit_application: {
+          applicant_id: applicant_id,
+          authorizer_id: authorizer_id,
+          plan_id: plan_id,
+          status: status,
+        },
+      }
 
-    
-    # memo
-    # ①VisitApplicationテーブルの"user_id"カラム名を"applicant_id"に変更
-    # ②VisitApplicationテーブルに"authorizer_id"カラムを追加
+      render(json: obj, status: 200)
+    else
+      render({msg: "failed ..."}, status: 500)
+    end
 
   end
 end
